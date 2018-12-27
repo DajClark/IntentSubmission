345f3dba72a5b03ed332adee000964cc7deaef1d
Daniel Clark <dajclark@googlemail.com>
2018-12-27 18:55:13 +0000

    finished adding intent functionality and event listeners to controller classes

--- app/src/main/java/com/example/intentsubmission/GameActivity.java
+++ app/src/main/java/com/example/intentsubmission/GameActivity.java
@@ -1,13 +1,122 @@
 package com.example.intentsubmission;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
 
+    // Private variables for game index array and request codes.
+    private String[] mGames;
+    private int mGameIndex = 0;
+    private static final int IS_SUCCESSFUL = 0;
+
+    // Tag for log messages.
+    public static final String TAG = "GameActivity";
+
+    // Key value name for value passed back in intent upon result.
+    private static final String IS_GAME_COMPLETE = "com.example.isGameComplete";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Call to super method to use super class onCreate methods.
         super.onCreate(savedInstanceState);
+
+        // Log message to show lifecycle state.
+        Log.d(TAG, " *** the PC is in onCreate ***");
+
+        // Sets activity view using XML layout file.
         setContentView(R.layout.activity_game);
+
+        // Initialises and sets the main text view to the appropriate array value.
+        final TextView gameTextView = findViewById(R.id.textViewGame);
+        Resources res = getResources();
+        mGames = res.getStringArray(R.array.game);
+        gameTextView.setText(mGames[mGameIndex]);
+
+        // Initialises previous button and listener to decrement array index.
+        Button buttonPrev = findViewById(R.id.buttonPrev);
+        buttonPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGameIndex = ((mGameIndex - 1) + mGames.length) % mGames.length;
+                gameTextView.setText(mGames[mGameIndex]);
+                gameTextView.setBackgroundColor(ContextCompat.getColor(GameActivity.this,R.color.backgroundDefault));
+                setTextViewComplete("");
+            }
+        });
+
+        // Initialises detail button and calls intent from GameDetailActivity and starts the activity for a result.
+        Button buttonDetail = findViewById(R.id.buttonDetail);
+        buttonDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = GameDetailActivity.newIntent(GameActivity.this, mGameIndex);
+                startActivityForResult(intent, IS_SUCCESSFUL);
+            }
+        });
+
+        // Initialises next button and listener to increment array index.
+        Button buttonNext = findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mGameIndex = (mGameIndex + 1) % mGames.length;
+                gameTextView.setText(mGames[mGameIndex]);
+                gameTextView.setBackgroundColor(ContextCompat.getColor(GameActivity.this,R.color.backgroundDefault));
+                setTextViewComplete("");
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        // Checks if request code is successful and calls the update view method or shows toast message.
+        if(requestCode == IS_SUCCESSFUL) {
+            if(intent != null) {
+
+                // Gathers the result from the intent and updates view accordingly.
+                boolean isGameComplete = intent.getBooleanExtra(IS_GAME_COMPLETE, false);
+                updateGameComplete(isGameComplete);
+            } else {
+                Toast.makeText(this, R.string.back_button_pressed, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, R.string.request_code_mismatch, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Method to update the activity view data if the value is passed that the game has been completed.
+    private void updateGameComplete(boolean isGameComplete) {
+
+        // Gets the main TextView by its id and sets the text and background colour to show the game has been set to completed.
+        final TextView textViewGame = findViewById(R.id.textViewGame);
+        if(isGameComplete) {
+            textViewGame.setBackgroundColor(ContextCompat.getColor(this,R.color.backgroundSuccess));
+            textViewGame.setTextColor(ContextCompat.getColor(this,R.color.colorSuccess));
+            setTextViewComplete("Complete");
+        } else {
+            textViewGame.setBackgroundColor(ContextCompat.getColor(this,R.color.backgroundDefault));
+            setTextViewComplete("");
+        }
+    }
+
+    // Method to set the appropriate game completion status.
+    private void setTextViewComplete(String message) {
+
+        // Gets and sets the completion TextView text using the TextView ID.
+        final TextView textViewComplete = findViewById(R.id.textViewComplete);
+        textViewComplete.setText(message);
     }
 }
--- app/src/main/java/com/example/intentsubmission/GameDetailActivity.java
+++ app/src/main/java/com/example/intentsubmission/GameDetailActivity.java
@@ -1,6 +1,85 @@
 package com.example.intentsubmission;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameDetailActivity extends AppCompatActivity {
+
+    // Key value names for passing values using intents.
+    private static final String GAME_INDEX = "com.example.gameIndex";
+    private static final String IS_GAME_COMPLETE = "com.example.isGameComplete";
+
+    // Intent for passing the game index position to the GameDetailActivity class.
+    public static Intent newIntent(Context packageContext, int gameIndex){
+        Intent intent = new Intent(packageContext, GameDetailActivity.class);
+        intent.putExtra(GAME_INDEX,gameIndex);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Calls super methods and sets the activity view using the XML layout file.
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_detail);
+
+        // Gets and sets the game index position from the intent.
+        int gameIndex = getIntent().getIntExtra(GAME_INDEX, 0);
+        updateTextViewGameDetail(gameIndex);
+
+        // Initialises and sets checkbox and listeners.
+        CheckBox checkboxIsComplete = findViewById(R.id.checkBoxIsComplete);
+        checkboxIsComplete.setOnClickListener(mGameListener);
+    }
+
+    private void updateTextViewGameDetail(int gameIndex) {
+
+        // Initialises the TextViews using the corresponding resource ID.
+        final TextView textViewGamePlatform = findViewById(R.id.textViewGamePlatform);
+        final TextView textViewGameDescription = findViewById(R.id.textViewGameDescription);
+
+        // Gets the non-code assets from resource interface to gather data from the XML Layout file.
+        Resources res = getResources();
+
+        // Initialises and sets the TextView values from the arrays in the XML layout file using their ID's.
+        String[] gamePlatform = res.getStringArray(R.array.game_platform);
+        String[] gameDetails = res.getStringArray(R.array.game_description);
+        textViewGamePlatform.setText(gamePlatform[gameIndex]);
+        textViewGameDescription.setText(gameDetails[gameIndex]);
+    }
+
+    // Listener for checkbox to check for checkbox status.
+    private View.OnClickListener mGameListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.checkBoxIsComplete:
+                    CheckBox checkboxIsComplete = findViewById(R.id.checkBoxIsComplete);
+                    setIsComplete(checkboxIsComplete.isChecked());
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    // Method to initialise intent with current checked status to return to onActivityResult in GameActivity class.
+    private void setIsComplete(boolean isChecked) {
+
+        if(isChecked){
+            Toast.makeText(GameDetailActivity.this,
+                    "The game is completed", Toast.LENGTH_LONG).show();
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(IS_GAME_COMPLETE, isChecked);
+        setResult(RESULT_OK, intent);
+    }
+
 }
